5157b29f3959196038cb66a4c3ab1e2408d67598
yunhacandy <yunhacandy@gmail.com>
2024-06-02 17:18:57 +0900

    Chore: initial commit

---
+++ src/test/java/org/cotato/growingpain/GrowingPainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cotato.growingpain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingPainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
