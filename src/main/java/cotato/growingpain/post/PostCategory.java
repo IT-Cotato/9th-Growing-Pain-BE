c62bdde832f66f181e4eafd51696b4b720f30735
yunhacandy <yunhacandy@gmail.com>
2024-06-24 15:13:27 +0900

    feat: community 기능 중 post 관련 엔티티에 기능 추가
    - Post 엔티티에 saveCount 필드 추가
    - PostSave 엔티티 추가
    - PostCategory enum에 카테고리 추가

--- src/main/java/cotato/growingpain/post/PostCategory.java
+++ src/main/java/cotato/growingpain/post/PostCategory.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 @Getter
 public enum PostCategory {
 
-    CATEGORY("카테고리"),
-    COMPETITION("공모전"),
+    ALL("전체"),
+    FREE("자유게시판"),
+    TEAM("팀원 모집"),
+    PORTFOLIO("포트폴리오"),
+    CONTEST("공모전"),
     PROJECT("프로젝트"),
     STUDY("스터디");
 
-    private final String key;
+    private final String description;
 
-    PostCategory(String key) {
-        this.key = key;
+    PostCategory(String description) {
+        this.description = description;
     }
 }
--- src/main/java/cotato/growingpain/post/domain/entity/PostSave.java
+++ src/main/java/cotato/growingpain/post/domain/entity/PostSave.java
@@ -3,65 +3,37 @@ package cotato.growingpain.post.domain.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import cotato.growingpain.common.domain.BaseTimeEntity;
 import cotato.growingpain.member.domain.Member;
-import cotato.growingpain.post.PostCategory;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
 @DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post extends BaseTimeEntity {
+public class PostSave extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
+    @Column(name = "post_save_id")
     private Long id;
-
-    private String title;
-
-    private String content;
-
-    @Column(name = "post_image_url")
-    private String imageUrl;
-
-    @Column(name = "post_category")
-    @Enumerated(EnumType.STRING)
-    @ColumnDefault(value = "'전체'")
-    private PostCategory category;
-
-    private int likeCount = 0;
-
-    private int saveCount = 0;
-
-    private boolean deleted = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     @JsonIgnore
     private Member member;
 
-    @OneToMany(mappedBy = "post")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
     @JsonIgnore
-    private List<PostLike> postLikes = new ArrayList<>();
-
-    @OneToMany(mappedBy = "post")
-    @JsonIgnore
-    private List<PostSave> postSaves = new ArrayList<>();
+    private Post post;
 }
