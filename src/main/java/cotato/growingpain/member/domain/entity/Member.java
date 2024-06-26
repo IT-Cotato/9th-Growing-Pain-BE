f7378ab8dc22a9385b57abc5d610472ff748f8ed
yunhacandy <yunhacandy@gmail.com>
2024-06-26 16:45:39 +0900

    feat: id 관련 import문 추가

--- src/main/java/cotato/growingpain/member/domain/entity/Member.java
+++ src/main/java/cotato/growingpain/member/domain/entity/Member.java
@@ -12,6 +12,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
