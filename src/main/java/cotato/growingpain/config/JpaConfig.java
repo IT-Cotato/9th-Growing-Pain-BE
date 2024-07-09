7c8aeb2fe365842bbf7505a6c7822bebe2dc55a9
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-07-09 16:05:45 +0900

    fix: @EnabledJpaRepositories 어노테이션 삭제

--- src/main/java/cotato/growingpain/config/JpaConfig.java
+++ src/main/java/cotato/growingpain/config/JpaConfig.java
@@ -2,10 +2,8 @@ package cotato.growingpain.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories
 public class JpaConfig {
 }
