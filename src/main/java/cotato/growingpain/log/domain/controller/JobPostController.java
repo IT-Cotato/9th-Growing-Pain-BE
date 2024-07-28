862f56888245e63023de481b2e93b24e5e1619a7
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-07-29 01:47:16 +0900

    docs: 스웨거 관련 어노테이션 추가

--- src/main/java/cotato/growingpain/log/domain/controller/JobPostController.java
+++ src/main/java/cotato/growingpain/log/domain/controller/JobPostController.java
@@ -5,15 +5,21 @@ import cotato.growingpain.log.domain.dto.JobPostRequestDTO;
 import cotato.growingpain.log.domain.dto.JobPostRetrieveDTO;
 import cotato.growingpain.log.domain.entity.JobPost;
 import cotato.growingpain.log.domain.service.JobService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -24,14 +30,20 @@ public class JobPostController {
 
     private final JobService jobService;
 
-    @PostMapping
+    @Operation(summary = "지원 현황 등록", description = "지원 현황을 등록하기 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
     public Response<JobPost> createJobPost(@RequestBody @Valid JobPostRequestDTO request,
                                            @AuthenticationPrincipal Long memberId) {
         jobService.createJobPost(request, memberId);
         return Response.createSuccess("지원 현황 등록 완료", null);
     }
 
-    @GetMapping
+    @Operation(summary = "지원 현황 조회", description = "지원 현황을 조회하기 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
     public Response<List<JobPostRetrieveDTO>> getJobPosts(
             @AuthenticationPrincipal Long memberId) {
         List<JobPostRetrieveDTO> retrievedJobPost = jobService.jobPostRetrieveList(memberId);
