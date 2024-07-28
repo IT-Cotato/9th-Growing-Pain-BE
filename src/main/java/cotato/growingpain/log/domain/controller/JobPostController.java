b8f2e87f92d43eacd9651ef545e796122a71a345
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-07-29 01:14:34 +0900

    refactor: 응답 형식 ResponseEntity -> Response로 변경

--- src/main/java/cotato/growingpain/log/domain/controller/JobPostController.java
+++ src/main/java/cotato/growingpain/log/domain/controller/JobPostController.java
@@ -1,5 +1,6 @@
 package cotato.growingpain.log.domain.controller;
 
+import cotato.growingpain.common.Response;
 import cotato.growingpain.log.domain.dto.JobPostRequestDTO;
 import cotato.growingpain.log.domain.dto.JobPostRetrieveDTO;
 import cotato.growingpain.log.domain.entity.JobPost;
@@ -8,7 +9,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,15 +25,15 @@ public class JobPostController {
     private final JobService jobService;
 
     @PostMapping
-    public ResponseEntity<JobPost> createJobPost(@RequestBody @Valid JobPostRequestDTO jobPostRequestDTO) {
+    public Response<JobPost> createJobPost(@RequestBody @Valid JobPostRequestDTO jobPostRequestDTO) {
         JobPost jobPost = jobService.createJobPost(jobPostRequestDTO);
-        return ResponseEntity.ok(jobPost);
+        return Response.createSuccess("지원 현황 등록 완료", null);
     }
 
     @GetMapping
-    public ResponseEntity<List<JobPostRetrieveDTO>> getJobPosts(
+    public Response<List<JobPostRetrieveDTO>> getJobPosts(
             @RequestParam Long memberId) {
         List<JobPostRetrieveDTO> retrievedJobPost = jobService.jobPostRetrieveList(memberId);
-        return ResponseEntity.ok(retrievedJobPost);
+        return Response.createSuccess("지원 현황 조회 완료", retrievedJobPost);
     }
 }
