145a13f955756e93e1bbda52a01ef69c73b725d4
yunhacandy <yunhacandy@gmail.com>
2024-07-29 22:16:17 +0900

    feat: 본인이 작성한 답글에는 좋아요 못하게 하는 기능 구현

--- src/main/java/cotato/growingpain/common/exception/ErrorCode.java
+++ src/main/java/cotato/growingpain/common/exception/ErrorCode.java
@@ -29,10 +29,10 @@ public enum ErrorCode {
     ALREADY_LIKED(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다."),
     POST_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시글의 좋아요 정보를 찾을 수 없습니다."),
     COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글의 좋아요 정보를 찾을 수 없습니다."),
-    REPLY_COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 답글의 좋아요 정보를 찾을 수 없습니다.");
     REPLY_COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 답글의 좋아요 정보를 찾을 수 없습니다."),
     CANNOT_LIKE_OWN_POST(HttpStatus.BAD_REQUEST, "본인이 작성한 게시글은 좋아요를 누를 수 없습니다."),
     CANNOT_LIKE_OWN_COMMENT(HttpStatus.BAD_REQUEST, "본인이 작성한 댓글은 좋아요를 누를 수 없습니다."),
+    CANNOT_LIKE_OWN_REPLY_COMMENT(HttpStatus.BAD_REQUEST, "본인이 작성한 답글은 좋아요를 누를 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
