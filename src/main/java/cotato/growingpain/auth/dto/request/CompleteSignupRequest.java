c5fdd2304179b70346ea48d48313936e39ae9567
yunhacandy <yunhacandy@gmail.com>
2024-08-12 21:28:25 +0900

    refactor: MemberJob 반드시 선택하도록 수정

--- src/main/java/cotato/growingpain/auth/dto/request/CompleteSignupRequest.java
+++ src/main/java/cotato/growingpain/auth/dto/request/CompleteSignupRequest.java
@@ -2,6 +2,7 @@ package cotato.growingpain.auth.dto.request;
 
 import cotato.growingpain.member.domain.MemberJob;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CompleteSignupRequest (
 
@@ -14,6 +15,7 @@ public record CompleteSignupRequest (
         @NotBlank(message = "소속은 필수 입력 항목입니다.")
         String belong,
 
+        @NotNull
         MemberJob job
 ) {
 }
