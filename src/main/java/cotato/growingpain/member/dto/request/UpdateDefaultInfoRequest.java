64e6035af91d6613a991027b914e8db58e1b8770
yunhacandy <yunhacandy@gmail.com>
2024-08-13 02:24:15 +0900

    fix: 에러로 인한 UpdateDefaultInfoRequest 제약 조건 삭제

--- src/main/java/cotato/growingpain/member/dto/request/UpdateDefaultInfoRequest.java
+++ src/main/java/cotato/growingpain/member/dto/request/UpdateDefaultInfoRequest.java
@@ -1,18 +1,13 @@
 package cotato.growingpain.member.dto.request;
 
 import cotato.growingpain.member.domain.MemberJob;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UpdateDefaultInfoRequest (
 
-        @NotBlank(message = "분야는 필수 입력 항목입니다.")
         String field,
 
-        @NotBlank(message = "소속은 필수 입력 항목입니다.")
         String belong,
 
-        @NotNull
         MemberJob job
 ) {
 }
