2599f70604fe855bd90d6329cf8e5ec33cbfe83c
yunhacandy <yunhacandy@gmail.com>
2024-08-13 15:32:26 +0900

    feat: 비밀번호 수정 기능 구현
    - 충돌 방지를 위해 비밀번호 초기화 기능 관련 네이밍 변경
    - 기존 비밀번호와 새로운 비밀번호 입력 시, 비밀번호가 수정되게 기능 구현 완료

--- src/main/java/cotato/growingpain/auth/dto/request/ChangePasswordRequest.java
+++ src/main/java/cotato/growingpain/auth/dto/request/ChangePasswordRequest.java
@@ -1,6 +1,13 @@
 package cotato.growingpain.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChangePasswordRequest(
-    String email
+
+        @NotBlank(message = "기존 비밀번호는 필수 입력 항목입니다.")
+        String currentPassword,
+
+        @NotBlank(message = "새 비밀번호는 필수 입력 항목입니다.")
+        String newPassword
 ) {
 }
